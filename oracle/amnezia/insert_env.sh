0c214ddd397acb6b95ca7c9564fd9d7946b37da3
Onion Yasmin <onionyas@student.21-school.ru>
2026-06-08 13:49:20 +0300

    feat: v.0.1, amneziaWG oracle, adding insert_env.sh

---
+++ oracle/amnezia/insert_env.sh
@@ -0,0 +1,8 @@
+tr -d '\r' < awg0.conf.tmp > awg0.conf.clean
+sed -i 's/\r//' .env
+
+export $(xargs < .env)
+
+envsubst '$PRIVATE_KEY_B_SERV $PUBLIC_KEY_A_SERV' < awg0.conf.clean > awg0.conf
+
+rm awg0.conf.clean
